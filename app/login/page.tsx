a3e2cdefba4499d495d9b0c52d613d431bc58e9d
Jiron.yang.liveklass <jiron.yang@liveklass.com>
2026-02-19 21:28:00 +0900

    fix:redirection hard coding

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${location.origin}/auth/callback`,
+        redirectTo: `https://pawsteps.vercel.app/auth/callback`,
       },
     })
   }
